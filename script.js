115fe1a7122e4284d7a57c54bbf136cede9e2fa0
emmaasha <emma.asha.kay@gmail.com>
2024-06-19 10:22:33 +0300

    calculator

---
+++ script.js
@@ -0,0 +1,35 @@
+const screen = document.getElementById('screen');
+
+function appendNumber(number) {
+    screen.value = screen.value === '0' ? number : screen.value + number;
+}
+
+function appendOperator(operator) {
+    if (screen.value === '') return;
+    if (isNaN(screen.value.slice(-1))) return;
+    screen.value += operator;
+}
+
+function calculate() {
+    try {
+        screen.value = eval(screen.value);
+    } catch {
+        screen.value = 'Error';
+    }
+}
+
+function clearScreen() {
+    screen.value = '';
+}
+
+function calculateSquareRoot() {
+    screen.value = Math.sqrt(screen.value);
+}
+
+function calculatepercent() {
+    screen.value = screen.value / 100;
+}
+
+function calculateBracketsButton() {
+    screen.value = '(' + screen.value + ')';
+}
